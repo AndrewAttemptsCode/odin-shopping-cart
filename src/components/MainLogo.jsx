b61bff4ecc4ae10e143a86ed0d0a5200514a8be1
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-12-30 18:07:06 +0000

    feat: adjust to accept props for logo dimensions, color and sizes

--- src/components/MainLogo.jsx
+++ src/components/MainLogo.jsx
@@ -1,4 +1,5 @@
 import { Store } from "lucide-react";
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
@@ -16,21 +17,35 @@ const Container = styled.div`
 
 const Text = styled.h1`
   color: #451a03;
+  font-size: ${({ $textSize }) => $textSize || "2rem"};
 `;
 
 const LogoLink = styled(Link)`
   text-decoration: none;
 `;
 
-export default function MainLogo() {
+const StyledIcon = styled(Store)`
+  color: ${({ $color }) => $color || "#451a03"};
+  width: ${({ $size }) => $size || "48px"};
+  height: ${({ $size }) => $size || "48px"};
+`;
+
+export default function MainLogo({ to="/", $size="48px", $color="#451a03", $textSize="2rem" }) {
   return (
-    <LogoLink to="/">
+    <LogoLink to={to}>
       <Container>
-        <Store color="#451a03" size={48} />
-        <Text>
+        <StyledIcon $color={$color} $size={$size} />
+        <Text $textSize={$textSize}>
           PopShop
         </Text>
       </Container>
     </LogoLink>
   );
 };
+
+MainLogo.propTypes = {
+  to: PropTypes.string,
+  $size: PropTypes.string,
+  $color: PropTypes.string,
+  $textSize: PropTypes.string
+}
