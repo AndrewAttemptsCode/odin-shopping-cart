4f2186a03e6dd6db7ecd871e710adf236f442d47
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-12-30 18:06:13 +0000

    add shop route

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,15 @@
 import App from './App'
+import Layout from './routes/Layout';
 
 const routes = [
   {
     path: "/",
     element: <App />,
   },
+  {
+    path: "/shop",
+    element: <Layout />,
+  }
 ];
 
 export default routes;
