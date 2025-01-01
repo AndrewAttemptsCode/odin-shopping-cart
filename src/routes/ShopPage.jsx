ece05679380e853503536ff90f286fa2cc473af3
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-01-01 15:43:54 +0000

    feat: receive shop items through outlet context and set up dummy data display

--- src/routes/ShopPage.jsx
+++ src/routes/ShopPage.jsx
@@ -1,7 +1,15 @@
+import { useOutletContext } from "react-router-dom";
+
 export default function ShopPage() {
+  const { shopItems } = useOutletContext();
+
   return (
     <>
       Shop page
+      {/* add conditional if shopitems > 0 if not add spinner */}
+      {shopItems.map((item) => (
+        <p key={item.id}>{item.title}</p>
+      ))}
     </>
   );
 };
