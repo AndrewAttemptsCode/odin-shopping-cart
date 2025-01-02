39b9a6c5460bc21b7517cffcf5c83da7d3703343
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-01-02 17:58:24 +0000

    feat: add container and shop card

--- src/routes/ShopPage.jsx
+++ src/routes/ShopPage.jsx
@@ -1,14 +1,31 @@
 import { useOutletContext } from "react-router-dom";
 import Spinner from "../components/Spinner";
+import ShopCard from "../components/ShopCard";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: grid;
+  gap: 2rem;
+  grid-template-columns: repeat(5, 1fr);
+  justify-content: center;
+  max-width: calc((200px * 5) + (4 * 2rem));
+  margin: 0 auto;
+`;
 
 export default function ShopPage() {
   const { shopItems } = useOutletContext();
 
   return (
-    <>
+    <Container>
       {shopItems.length > 0 ? shopItems.map((item) => (
-        <p key={item.id}>{item.title}</p>
+        <ShopCard
+          key={item.id}
+          src={item.image}
+          title={item.title}
+          price={item.price}
+        />
       )) : <Spinner />}
-    </>
+      
+    </Container>
   );
 };
