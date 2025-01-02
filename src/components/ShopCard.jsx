07ad8722bc38759cdd233100064998817a9f6e71
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-01-02 18:35:34 +0000

    feat: add initial shop item card design

---
+++ src/components/ShopCard.jsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+  border-radius: 5px;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.2);
+  background-color: #fefefe;
+`;
+
+const ProductImage = styled.img`
+  object-fit: contain;
+  object-position: center;
+  width: 200px;
+  height: 200px;
+`;
+
+export default function ShopCard({ src, title, price }) {
+  return (
+    <CardContainer>
+      <ProductImage src={src} />
+    </CardContainer>
+  );
+};
+
+// {
+//   "id": 1,
+//   "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+//   "price": 109.95,
+//   "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+//   "category": "men's clothing",
+//   "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+//   "rating": {
+//     "rate": 3.9,
+//     "count": 120
+//   }
