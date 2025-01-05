1304638ba5b493e87e742b26b345c99fce729228
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-01-05 17:14:15 +0000

    feat: pass disabled prop to Trolley

--- src/routes/Layout.jsx
+++ src/routes/Layout.jsx
@@ -73,7 +73,7 @@ export default function Layout() {
     <GridContainer>
       <Header>
         <MainLogo />
-        <Trolley totalItems={totalItems} totalPrice={totalPrice} />
+        <Trolley totalItems={totalItems} totalPrice={totalPrice} disabled={totalItems < 1} />
         {/* logo
         nav bar
         trolley button with slideout */}
